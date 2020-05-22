9b7511a4f6eaa4d68859280fdf517ef766281546
Mikita Iwanowski <info@slnt-opp.xyz>
2020-05-22 13:38:30 +0200

    Array.prototype.forEach

--- array.js
+++ array.js
@@ -1,6 +1,8 @@
 // Array.prototype.map
 // Array.prototype.filter
-// Array.prototype.forEach
+Array.prototype.forEach = function(callback, thisArg = this){
+    for(let i = 0; i < this.length; i++) callback.call(thisArg, this[i], i, this);
+}
 Array.prototype.reduce = function(accumulator, result){
     let i = 0;
     if(result == undefined) { result = this[0]; i = 1 };
