71277e17999e6774114290af774c1e0271ff8d31
Mikita Iwanowski <info@slnt-opp.xyz>
2020-05-22 13:57:00 +0200

    Array.prototype.filter

--- array.js
+++ array.js
@@ -1,9 +1,14 @@
 // Array.prototype.map
-// Array.prototype.filter
+Array.prototype.filter  = function(callback, thisArg = this){
+    return this.reduce((result, el, i, arr) => {
+        if(callback.call(thisArg, el, i, arr)) result.push(el);
+        return result;
+    }, [])
+}
 Array.prototype.forEach = function(callback, thisArg = this){
     for(let i = 0; i < this.length; i++) callback.call(thisArg, this[i], i, this);
 }
-Array.prototype.reduce = function(accumulator, result){
+Array.prototype.reduce  = function(accumulator, result){
     let i = 0;
     if(result == undefined) { result = this[0]; i = 1 };
     for(; i < this.length; i++) result = accumulator(result, this[i], i, this);
