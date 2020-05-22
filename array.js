daa94e0d69de4ded21b8186a758d8edcd64baa08
Mikita Iwanowski <info@slnt-opp.xyz>
2020-05-22 13:14:35 +0200

    Array.prototype.reduce

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 // Array.prototype.map
 // Array.prototype.filter
 // Array.prototype.forEach
-Array.prototype.myreduce = function(accumulator, result){
+Array.prototype.reduce = function(accumulator, result){
     let i = 0;
     if(result == undefined) { result = this[0]; i = 1 };
     for(; i < this.length; i++) result = accumulator(result, this[i], i, this);
